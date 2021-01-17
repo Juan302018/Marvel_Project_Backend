f18681d490b54a0e8c945bb6ae16cb8dcfa2a2fb
camilomendez1705 <cmendez1705@gmail.com>
2021-01-17 05:28:05 -0300

    se agrega la anotacion service a las clases de: -comicServiceImpl -peliculaServiceImpl

--- cl_marvel_superhero/src/main/java/cl/marvel/superhero/services/impl/ComicServiceImpl.java
+++ cl_marvel_superhero/src/main/java/cl/marvel/superhero/services/impl/ComicServiceImpl.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import cl.marvel.superhero.model.Comic;
 import cl.marvel.superhero.repo.IComicRepo;
 import cl.marvel.superhero.services.IComicService;
 
+@Service
 public class ComicServiceImpl implements IComicService{
 	
 	@Autowired
--- cl_marvel_superhero/src/main/java/cl/marvel/superhero/services/impl/PeliculasServiceImpl.java
+++ cl_marvel_superhero/src/main/java/cl/marvel/superhero/services/impl/PeliculasServiceImpl.java
@@ -4,12 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import cl.marvel.superhero.model.Pelicula;
 import cl.marvel.superhero.repo.IComicRepo;
 import cl.marvel.superhero.repo.IPeliculaRepo;
 import cl.marvel.superhero.services.IPeliculaService;
 
+@Service
 public class PeliculasServiceImpl implements IPeliculaService{
 	
 	@Autowired
